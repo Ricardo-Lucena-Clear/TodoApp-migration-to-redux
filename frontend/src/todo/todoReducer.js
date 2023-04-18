b0c4f5cd0397ac29593bffe01eeff15c693449f6
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-18 10:55:36 -0300

    Applying Middleware (Redux-Promise)

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-    description: "ler livro",
-    list: [{
-        _id: 1,
-        description: "pagar fatura",
-        done: true
-    }, {
-        _id: 2,
-        description: "reuniao com a equipé as 10",
-        done: false
-    }, {
-        id: 3,
-        description: "consulta medica",
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: "", list: [] }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type){
         case 'DESCRIPTION_CHANGED':
