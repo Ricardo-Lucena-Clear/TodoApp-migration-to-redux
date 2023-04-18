1937c39924f5adeb7de1d03c70b2e35aa4ed5f1a
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-18 11:11:04 -0300

    Add TODO

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { changeDescription, search } from "./todoActions";
+import { add,changeDescription, search } from "./todoActions";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
@@ -14,13 +14,15 @@ class TodoForm extends Component {
         this.props.search()
     }
     keyHandler(e){
+        const  {add, search, description} =this.props
         if(e.key === 'Enter'){
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         }else if(e.key === 'Escape'){
            this.props.handleClear()
         }
     }
     render (){
+        const  {add, search, description} =this.props
         return (
             <div role="form" className="todoForm">
 
@@ -34,9 +36,9 @@ class TodoForm extends Component {
 
         <Grid cols="12 3 2">
             <IconButton style="primary" icon="plus"
-            onClick={this.props.handleAdd}></IconButton>
+            onClick={()=> add(description)}></IconButton>
             <IconButton style="info" icon="search"
-            onClick={this.props.handleSearch}></IconButton>
+            onClick={()=> search()}></IconButton>
             <IconButton style="default" icon="close"
             onClick={this.props.handleClear}></IconButton>
         </Grid>
@@ -46,6 +48,6 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state=> ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({changeDescription, search}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({add,changeDescription, search}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
     
